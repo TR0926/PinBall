8206d3ce275b0b4c158723039254885d01308ae8
TR0926 <72895733+TR0926@users.noreply.github.com>
2020-11-04 18:25:33 +0900

    Initial commit

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreController : MonoBehaviour
+{
+    private int score;
+    private string scoreText;
+    private GameObject ScoreText;
+
+
+    private void Start()
+    {
+        this.ScoreText = GameObject.Find("Score");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 50;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += 20;
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += 30;
+        }
+    }
+
+    void Update()
+    {
+        scoreText = score.ToString();
+        this.ScoreText.GetComponent<Text>().text = scoreText;
+    }
+}
